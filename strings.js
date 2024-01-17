b2fca17686f68e68c6d0fa09c59535a963f6172e
Japnoor Singh <109523523+JapnoorHere@users.noreply.github.com>
2024-01-17 23:43:03 +0530

    change

--- strings.js
+++ strings.js
@@ -1,2 +1,25 @@
+// ! Strings are immutable 
+// ^  values will not change in string variable
+
+
 let s="Japnoor";
 console.log(s);
+
+console.log(`I am ${s}`); // * backtic -> ` `
+
+
+let f = "HelloHel";
+
+console.log(f.toUpperCase());
+console.log(f.toLowerCase());
+console.log(f.length);
+
+console.log(f.slice(0,3));
+console.log(f.slice(1)); // !prints all from 1 index
+
+console.log(f.replace("Hel","i")); // ^ replace only first occurence
+
+
+console.log(f.concat(s,"Hi","oh no  ","Siya")); // * keep on adding for concatenation
+
+
