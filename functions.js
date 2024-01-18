94a45a7cd6b58ee5fbbeae5432e83a47c3ce2359
Japnoor Singh <109523523+JapnoorHere@users.noreply.github.com>
2024-01-18 22:22:03 +0530

    learnt

--- functions.js
+++ functions.js
@@ -4,12 +4,25 @@ function a() {
 
 // console.log(a());
 
+
+
+//* DIFFERENT WAYS TO WRITE FUNCTIONS
+
+let add = function (a,b){
+  return a+b;
+}
+
+console.log("sum : " + add(4,5));
+
+
+
+
 const fact = (n)=>{
-    let f = 1;
-    for(let i=1;i<=n;i++){
-        f *= i;
-    }
-    return f;
+  let f = 1;
+  for(let i=1;i<=n;i++){
+      f *= i;
+  }
+  return f;
 }
 
 console.log(fact(5));
