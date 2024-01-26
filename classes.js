23e5c125d6eb978172fdb6a724638f6db0b717c1
Japnoor Singh <109523523+JapnoorHere@users.noreply.github.com>
2024-01-27 01:32:55 +0530

    learnt

---
+++ classes.js
@@ -0,0 +1,15 @@
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const person1 = new Person("Sham",12);
+const person2 = new Person("Ram",15);
+
+console.log(typeof person1);
+
+console.log(person1.name);
+
+console.log(person2.age);
